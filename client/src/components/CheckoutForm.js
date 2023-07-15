adfb5e9220e821da1bcc172b6557363360b09ea9
kelleman <kellemanatser@gmail.com>
2023-07-15 14:57:23 +0100

    .

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -20,7 +20,7 @@ function CheckoutForm() {
         e.preventDefault();
         if (!stripe || !elements || user.cart.count <= 0) return;
         setPaying(true);
-        const { client_secret } = await fetch("http://localhost:8080/create-payment", {
+        const { client_secret } = await fetch("https://gta-apis.onrender.com/create-payment", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
